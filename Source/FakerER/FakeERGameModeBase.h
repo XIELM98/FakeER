9a22ac31c2e504aaf54cbd75d9ec1ce48ab3ecea
XIELM98 <164745389+XIELM98@users.noreply.github.com>
2024-03-24 20:02:59 +0800

    Add files via upload

---
+++ Source/FakerER/FakeERGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FakeERGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FAKEER_API AFakeERGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
